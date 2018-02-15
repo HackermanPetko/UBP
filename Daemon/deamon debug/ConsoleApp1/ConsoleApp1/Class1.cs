fa79d18d5a1cf7bffa232e1ab270a90e31e46e2f
CZprofi <profiiforp@gmail.com>
2018-02-15 21:59:29 +0100

    vytvoření daemona a test získávání configu

---
+++ Daemon/deamon debug/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Config
+    {
+        public int idConfig { get; set; }
+
+        public int idDaemon { get; set; }
+
+        public int BackupType { get; set; }
+
+        public string DestinationType { get; set; }
+
+        public string DestinationAddress { get; set; }
+
+        public int FTPport { get; set; }
+
+        public string DestinationPassword { get; set; }
+
+        public string DestinationUser { get; set; }
+
+        public int Format { get; set; }
+
+        public bool Repeatable { get; set; }
+
+        public int Interval { get; set; }
+
+        public DateTime LastChecked { get; set; }
+
+        private Config()
+        {
+
+        }
+
+        private static async Task<Config> LoadConfig(int id)
+        {
+            HttpClient client = new HttpClient();
+
+            string json = await client.GetStringAsync($"http://localhost:63699/api/config/{id}");
+
+            Config config = JsonConvert.DeserializeObject<Config>(json);
+
+            //this.idConfig = config.idConfig;
+            //this.idDaemon = config.idDaemon;
+            //this.BackupType = config.BackupType;
+            //this.DestinationType = config.DestinationType;
+            //this.DestinationAddress = config.DestinationAddress;
+            //this.FTPport = config.FTPport;
+            //this.DestinationPassword = config.DestinationPassword;
+            //this.DestinationUser = config.DestinationUser;
+            //this.Format = config.Format;
+            //this.Repeatable = config.Repeatable;
+            //this.Interval = config.Interval;
+            //this.LastChecked = config.LastChecked;
+
+            return config;
+
+        }
+
+        public static Config GetConfig(int id)
+        {
+            var task = LoadConfig(id);
+            task.Wait();
+            Config config = task.Result;
+
+            return config;
+        }
+    }
+}
---
+++ Daemon/deamon debug/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Config config = Config.GetConfig(2);
+
+
+            Console.WriteLine($"{config.idConfig},{config.Repeatable}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
