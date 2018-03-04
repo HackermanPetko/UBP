f6c26bbc072936d1917682c47b272ad0cf873478
Daniel Blažek <profiiforp@gmail.com>
2018-03-04 21:23:52 +0100

    fullbackup ftp
    i s ignorovanýma souborama..

---
+++ Daemon/deamon debug/ConsoleApp1/ConsoleApp1/BackupTypes/FTPBackup.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.BackupTypes
+{
+    public class FTPBackup 
+    {
+        public void FullBackup(string source, string destination, string port, string user, string password,string date)
+        {
+            DirectoryInfo dirSource = new DirectoryInfo(source);
+
+
+
+            string uri = "ftp://" + destination + ":" + port +  "/" + dirSource.Name + "/" + date;
+
+            FtpWebRequest reqFTP;
+
+            
+            reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(uri));
+
+            NetworkCredential credentials = new NetworkCredential(user, password);
+            
+
+            this.CopyAllToFTP(dirSource, uri, reqFTP,credentials);
+        }
+
+        public void DifferentialBackup(string source, string destination)
+        {
+
+        }
+
+        public void IncrementalBackup(string source, string destination)
+        {
+
+        }
+
+        private void CopyAllToFTP(DirectoryInfo dirSource, string uri, FtpWebRequest reqFTP, NetworkCredential credentials)
+        {
+
+            Stream ftpStream = null;
+            reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(uri));
+            reqFTP.Method = WebRequestMethods.Ftp.MakeDirectory;
+            reqFTP.Credentials = credentials;
+            reqFTP.UsePassive = false;
+            reqFTP.UseBinary = true;
+            FtpWebResponse response = (FtpWebResponse)reqFTP.GetResponse();
+            ftpStream = response.GetResponseStream();
+            ftpStream.Close();
+            response.Close();
+
+
+            foreach (FileInfo file in dirSource.GetFiles())
+            {
+                this.CopyFileToFTP(file, uri + "/" + file.Name, credentials);
+            }
+
+            foreach (DirectoryInfo dir in dirSource.GetDirectories())
+            {
+                this.CopyAllToFTP(dir, uri + "/" + dir.Name, reqFTP,credentials);
+            }
+        }
+
+        private void CopyFileToFTP(FileInfo file,string uri, NetworkCredential credentials)
+        {
+            FtpWebRequest ftpWebRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri(uri));
+            ftpWebRequest.ContentLength = file.Length;
+            ftpWebRequest.Credentials = credentials;
+            ftpWebRequest.UsePassive = false;
+            ftpWebRequest.UseBinary = true;
+            ftpWebRequest.Method = WebRequestMethods.Ftp.UploadFile;
+
+            int buffLength = 2048;
+            byte[] buff = new byte[buffLength];
+            int contentLen;
+
+            FileStream fs = file.OpenRead();
+
+            Stream strm = ftpWebRequest.GetRequestStream();
+            contentLen = fs.Read(buff, 0, buffLength);
+
+            while (contentLen != 0)
+            {
+                strm.Write(buff, 0, contentLen);
+                contentLen = fs.Read(buff, 0, buffLength);
+            }
+
+            strm.Close();
+            fs.Close();
+
+        }
+    }
+}
---
+++ Daemon/deamon debug/ConsoleApp1/ConsoleApp1/BackupTypes/LocalBackup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.BackupTypes
+{
+    public class LocalBackup
+    {
+        public void FullBackup(string source, string destination,string date)
+        {
+            DirectoryInfo dirSource = new DirectoryInfo(source);
+            DirectoryInfo dirDest = new DirectoryInfo(destination);
+
+            this.CopyAll(dirSource, dirDest.CreateSubdirectory(date).CreateSubdirectory(dirSource.Name));
+        }
+
+        public void DifferentialBackup(string source, string destination)
+        {
+
+        }
+
+        public void IncrementalBackup(string source, string destination)
+        {
+
+        }
+
+
+        //kopírování všech souborů 
+        private void CopyAll(DirectoryInfo source, DirectoryInfo destination)
+        {
+            foreach (FileInfo file in source.GetFiles())
+            {
+                file.CopyTo(Path.Combine(destination.FullName, file.Name), true);
+            }
+
+            foreach (DirectoryInfo dir in source.GetDirectories())
+            {
+                DirectoryInfo newdir = destination.CreateSubdirectory(dir.Name);
+                this.CopyAll(dir, newdir);
+            }
+        }
+    }
+}
