1701654ec57aada23efa11fba7de9de32371f1c7
Daniel Blažek <profiiforp@gmail.com>
2018-03-27 10:42:58 +0200

    Příprava BCryptu

---
+++ Server - API/ServerAPI/ServerAPI/Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ServerAPI.Models;
+
+namespace ServerAPI.Controllers
+{
+    public class RegisterController : ApiController
+    {
+        private TestContext context = new TestContext();
+       
+        // POST: api/Register
+        public void Post(User user)
+        {
+            
+
+
+
+        }
+
+       
+    }
+}
---
+++ Server - API/ServerAPI/ServerAPI/Models/Encrypter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace ServerAPI.Models
+{
+    public class Encrypter
+    {
+
+        static string myPassword="0";
+        static string mySalt = "$2a$10$rBV2JDeWW3.vKyeQcM8fFO";
+        string myHash = "0";
+
+       
+        //mySalt == "$2a$10$rBV2JDeWW3.vKyeQcM8fFO"
+        static string myHash = BCrypt.Net.BCrypt.HashPassword(myPassword, mySalt);
+        //myHash == "$2a$10$rBV2JDeWW3.vKyeQcM8fFO4777l4bVeQgDL6VIkxqlzQ7TCalQvla"
+        bool doesPasswordMatch = BCrypt.Net.BCrypt.Verify(myPassword,myHash);
+
+
+    }
+}
