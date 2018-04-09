9bb0b9f41bd9de194907ec825135e0c7cfde6966
XpellCZ <poul.marek@gmail.com>
2018-04-09 22:56:48 +0200

    API - tasky, sources, destinations

---
+++ Server - API/ServerAPI/ServerAPI/Controllers/SourceController.cs
@@ -0,0 +1,76 @@
+﻿using ServerAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ServerAPI.Controllers
+{
+
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class SourceController : ApiController
+    {
+        private TestContext context;
+
+        public SourceController()
+        {
+            this.context = new TestContext();
+        }
+
+        // GET: api/Source/5
+        public List<Source> Get(int id)
+        {
+            List<Source> sources = this.context.Sources.ToList();
+            List<Source> result = new List<Source>();
+
+            foreach (Source src in sources)
+            {
+                if (src.IdTask == id)
+                {
+                    result.Add(src);
+                    continue;
+                }
+
+            }
+
+            return result;
+
+        }
+
+        // POST: api/Source
+        public void Post(Source source)
+        {
+            Source temp = this.context.Sources.Find(source.Id);
+
+            if (temp != null)
+            {
+                temp.Id = source.Id;
+                temp.IdTask = source.IdTask;
+                temp.SourcePath = source.SourcePath;
+
+            }
+            else
+            {
+                this.context.Sources.Add(source);
+            }
+
+            this.context.SaveChanges();
+
+
+
+        }
+
+        //// PUT: api/Source/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/Source/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Server - API/ServerAPI/ServerAPI/Controllers/TaskController.cs
@@ -0,0 +1,73 @@
+﻿using ServerAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ServerAPI.Controllers
+{
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class TaskController : ApiController
+    {
+        private TestContext context;
+
+        public TaskController()
+        {
+            this.context = new TestContext();
+        }
+
+        //// GET: api/Task
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET: api/Task/5
+        public List<BackupTask> Get()
+        {
+           
+            return this.context.Tasks.ToList();
+
+        }
+
+        // POST: api/Task
+        public void Post(BackupTask task)
+        {
+            BackupTask temp = this.context.Tasks.Find(task.Id);
+
+            if (temp != null)
+            {
+                temp.BackupType = task.BackupType;
+                temp.Format = task.Format;
+                temp.Id = task.Id;
+                temp.IdConfig = task.MaxBackups;
+                temp.RepeatInterval = task.RepeatInterval;
+                
+
+
+            }
+            else
+            {
+                this.context.Tasks.Add(task);
+            }
+
+            this.context.SaveChanges();
+
+
+
+        }
+
+        //// PUT: api/Task/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/Task/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
